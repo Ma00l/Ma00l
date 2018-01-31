de14b96f34a967f18262770b87f2d96de7a16170
cnleo <cnleo@cosmicnova.de>
2018-01-31 17:46:44 +0100

    Init

---
+++ README.md
@@ -0,0 +1,55 @@
+# Nvidia Telemetrie Entfernen
+
+Ein kleines Batchscript um die Telemetrie von Nvidia abzuschalten bzw. etwas einzudämmen. 
+Es folgt zum Teil den Anweisungen, wie sie bei http://extreme.pcgameshardware.de/grafikkarten/458638-anleitung-nvidia-telemetrie-deaktivieren.html aufgezeigt wurden (siehe auch Programmablauf).
+
+Das Script ist nur für Windows 7 (getestet) und Vista (nicht getestet) sowohl 32 Bit als auch 64 Bit.
+Es sollte als Administrator ausgeführt werden um die Hosts-Datei editieren zu können.
+
+Das Script führt durch die einzelnen Schritte.
+
+
+## Alternative und Ergänzende Tools
+
+Es gibt ein Tool das etwas mehr Komfort bieten und wohl auch für neuere Betriebssysteme funktioniert. Es fehlen allerdings die Ergänzungen in der Hosts-Datei:
+https://github.com/NateShoffner/Disable-Nvidia-Telemetry
+
+Zusätzlich gibt es noch ein Batchscript das die ganzen „überschüssigen/bloatware“ Sub-Ordner im Nvidia Installations-Ordners entfernt. Also die Treiber erst extrahieren, dann mit dem Chef-Koch Script entfernen, dann die Treiber Installieren.
+https://gist.github.com/CHEF-KOCH/489686c18b709f064be9eb5180880b9f
+
+
+## Programmablauf
+
+
+### Deaktiviert 3 Einträge im Taskmanager | der Aufgabenplanung
+
+NvTmMon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}
+NvTmRep_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}
+NvTmRepOnLogon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}
+
+### Aufruf der Deinstallationsroutine von Nvidia zum deinstallieren des NvTelemetryContainer
+
+rundll32 "%PROGRAMFILES%\NVIDIA Corporation\Installer2\InstallerCore\NVI2.DLL",UninstallPackage NvTelemetryContainer
+
+
+### Hosts-Datei Einträge Ergänzungen
+
+127.0.0.1 gfwsl.geforce.com  
+127.0.0.1 gfe.geforce.com  
+127.0.0.1 telemetry.nvidia.com  
+127.0.0.1 gfe.nvidia.com  
+127.0.0.1 telemetry.gfe.nvidia.com  
+127.0.0.1 events.gfe.nvidia.com  
+::1 gfwsl.geforce.com  
+::1 gfe.geforce.com  
+::1 telemetry.nvidia.com  
+::1 gfe.nvidia.com  
+::1 telemetry.gfe.nvidia.com  
+::1 events.gfe.nvidia.com  
+
+
+## Allgemein
+
+Auch hier sei noch mal gesagt. Installieren Sie die Nvidia Treiber nicht via Express und wählen Sie alles bis auf die Grafik-Treiber selber ab bzw. je nach persönlicher Präferenz weitere dazu.
+
+Führen Sie nach jedem erneuten Installieren von Nvidia Treiber dieses Script aus. Sie können sich allerdings dann den Part der Bearbeitung der Hosts-Datei sparen. Zumindest unter Windows 7 ist mir nicht bekannt, dass die Hosts-Datei durch Windows Updates resettet wird.
---
+++ nvidia-telemetry-remove.cmd
@@ -0,0 +1,157 @@
+@ECHO OFF
+REM setlocal EnableDelayedExpansion
+REM chcp 65001
+CHCP 1250
+CLS
+
+REM  ISC License Copyright ^(c^) 2018^, cnleo
+REM  https://github.com/cnleo
+REM  https://choosealicense.com/licenses/isc/
+ECHO(
+
+REM  LESEN:
+ECHO(
+ECHO  Umlaute: AE ^= Ä^, OE ^= Ö^, UE ^= Ü^, SS ^= ß
+ECHO  Wenn sie keine Umlaute erkennen^,
+ECHO  aendern sie bitte die Schrift zu Lucida Console.
+ECHO  Am oberen Fensterrand druecken Sie:
+ECHO  Rechtsklick --^> Eigenschaften --^> Schrift
+ECHO(
+ECHO  Dieses Script ist ausschließlich für Windows 7 ^(getestet^)
+ECHO  und Vista ^(nicht getestet^).
+ECHO(
+ECHO  Dieses Script muss als Administrator ausgeführt werden^,
+ECHO  damit die HOSTS-Datei editiert werden kann.
+ECHO  Anstelle eines Doppelklicks auf diese Datei:
+ECHO  Rechtsklick --^> Als Administrator ausführen...
+
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+REM  LESEN:
+ECHO(
+ECHO  Es werden jetzt drei Tasks deaktiviert.
+ECHO  Die Ausgabe sollte jeweils eine der folgenden sein:
+ECHO  FEHLER: Der angegebene Aufgabenname xyz ist auf dem System nicht vorhanden.
+ECHO  Oder ERFOLGREICH: Die Parameter der geplanten Aufgabe xyz wurden geändert. 
+  
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+ECHO  STATUS:
+ECHO(
+ECHO  Task NvTmMon wird deaktiviert^, sofern vorhanden ...
+schtasks /change /TN NvTmMon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+ECHO  ... fertig.
+ECHO(
+ECHO  Task NvTmRep wird deaktiviert^, sofern vorhanden ...
+schtasks /change /TN NvTmRep_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+ECHO  ... fertig.
+ECHO(
+ECHO  Task NvTmRepOnLogon wird deaktiviert^, sofern vorhanden ...
+schtasks /change /TN NvTmRepOnLogon_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8} /disable
+ECHO ... fertig.
+
+ECHO(
+PAUSE 
+CLS
+ECHO(
+
+REM  LESEN:
+ECHO(
+ECHO  Es wird nun der Telemetrie Dienst Deinstalliert.
+ECHO  Dazu eröffnet sich ein Fenster von Nvidia^, 
+ECHO  es sei denn es ist bereits Deinstalliert.
+ECHO  Um anschließend hier fortzufahren müssen sie ggf. das Nvidia Fenster
+ECHO  nach der Deinstallation des Telemetrie Containers Schließen.
+
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+ECHO  STATUS:
+ECHO(
+ECHO  Um hier fortfahren zu können muss das Nvidia Fenster geschlossen werden^, 
+ECHO  sofern der Telemetrie-Container noch nicht Deinstalliert wurde ...
+rundll32 "%PROGRAMFILES%\NVIDIA Corporation\Installer2\InstallerCore\NVI2.DLL",UninstallPackage NvTelemetryContainer
+ECHO  ... fertig.
+
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+REM  LESEN:
+ECHO  Zu guter Letzt werden in der HOSTS Datei noch die 
+ECHO  Telemetrie-Server-Adressen zum blocken hinzugefügt.
+ECHO  Zunächst sollten sie aber prüfen^, ob bereits ein Nvidia Eintrag existiert.
+ECHO  Wenn ja sind sie fertig und können dieses Fenster einfach schließen.
+ECHO  Andernfalls schließen Sie bitte Notepad mit der Hosts Datei wieder^, 
+ECHO  die sich automatisch für sie öffnete und fahren hier fort.
+notepad "%SystemRoot%\system32\drivers\etc\hosts" 
+
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+ECHO  STATUS:
+ECHO  ...
+ECHO(>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO # nVidia Telemetry begin>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 gfwsl.geforce.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 gfe.geforce.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 telemetry.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 telemetry.gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO 127.0.0.1 events.gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 gfwsl.geforce.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 gfe.geforce.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 telemetry.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 telemetry.gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO ::1 events.gfe.nvidia.com>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO # nVidia Telemetry end>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO(>>"%SystemRoot%\system32\drivers\etc\hosts"
+ECHO  ... fertig.
+
+ECHO(
+PAUSE
+CLS
+ECHO(
+
+REM  LESEN:
+ECHO(
+ECHO  Sie sind fertig und können dieses Fenster einfach schließen oder 
+ECHO  ein letztes weiteres Mal eine beliebige Taste drücken.
+
+
+
+ECHO(
+
+ECHO  MEHR INFOS:
+ECHO(
+REM  https://en.wikibooks.org/wiki/Windows_Programming/Programming_CMD
+REM  https://en.wikipedia.org/wiki/Hosts_(file)
+REM  http://www.robvanderwoude.com/schtasks.php
+REM  https://ss64.com/nt/echo.html
+REM  http://www.robvanderwoude.com/escapechars.php
+ECHO  http://extreme.pcgameshardware.de/grafikkarten/458638-anleitung-nvidia-telemetrie-deaktivieren.html
+ECHO(
+ECHO  ANDERE TOOLS:
+ECHO(
+ECHO  https://github.com/NateShoffner/Disable-Nvidia-Telemetry
+ECHO  https://gist.github.com/CHEF-KOCH/489686c18b709f064be9eb5180880b9f
+
+ECHO(
+
+REM Keep window open with pause
+
+PAUSE
+
+@ECHO ON
